dd13413486a4dd9e88cc81f146df35dc85b3c693
BHIN <vksek375@gmail.com>
2025-02-21 19:56:02 +0900

    4차

---
+++ Update_Resource_ShaderFile.bat
@@ -0,0 +1,7 @@
+
+
+xcopy			/y/s			.\Resource_ShaderFile\Resources\*.*								.\Client\Bin\Resources\
+
+xcopy			/y/s			.\Resource_ShaderFile\ShaderFile\*.*							.\Client\Bin\ShaderFiles\
+
+xcopy			/y/s			.\Resource_ShaderFile\Engine_ShaderFile\*.*						.\Engine\Bin\ShaderFiles\
